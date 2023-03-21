9a1d049d49d11a1c7699fe2fd1f00d632d750669
ImMohammadHosseini <sayedmh50@gmail.com>
2023-03-21 14:10:37 +0330

    complete mp_encoder class

--- src/models/mp_encoder.py
+++ src/models/mp_encoder.py
@@ -1,8 +1,49 @@
 
 import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+
+class Attention(nn.Module):
+    def __init__(self, hidden_dim, attn_drop):
+        super(Attention, self).__init__()
+        self.fc = nn.Linear(hidden_dim, hidden_dim, bias=True)
+        nn.init.xavier_normal_(self.fc.weight, gain=1.414)
+
+        self.tanh = nn.Tanh()
+        self.att = nn.Parameter(torch.empty(size=(1, hidden_dim)), requires_grad=True)
+        nn.init.xavier_normal_(self.att.data, gain=1.414)
+
+        self.softmax = nn.Softmax()
+        if attn_drop:
+            self.attn_drop = nn.Dropout(attn_drop)
+        else:
+            self.attn_drop = lambda x: x
+
+    def forward(self, embeds):
+        beta = []
+        attn_curr = self.attn_drop(self.att)
+        for embed in embeds:
+            sp = self.tanh(self.fc(embed)).mean(dim=0)
+            beta.append(attn_curr.matmul(sp.t()))
+        beta = torch.cat(beta, dim=-1).view(-1)
+        beta = self.softmax(beta)
+        print("mp ", beta.data.cpu().numpy())  # semantic attention
+        z_mp = 0
+        for i in range(len(embeds)):
+            z_mp += embeds[i]*beta[i]
+        return z_mp
 
 
 class Mp_encoder(nn.Module):
-    def __init__(self, P, hidden_dim, attn_drop):
+    def __init__ (self, P, hidden_dim, attn_drop):
         super(Mp_encoder, self).__init__()
-
+        self.P = P
+        self.gcn_layers = nn.ModuleList([GCNConv(hidden_dim, hidden_dim) for _ in range(P)])
+        self.attention = Attention(hidden_dim, attn_drop)
+    def forward (self, h, mps):
+        embeds = []
+        for i in range(self.P):
+            embeds.append(self.node_level[i](h, mps[i]))
+        z_mp = self.att(embeds)
+        return z_mp
+        

4a284815e1c928db35383af84fdb810a55dce153
ImMohammadHosseini <sayedmh50@gmail.com>
2023-03-25 17:10:09 +0330

    add mp and sc parameters

--- src/models/heco.py
+++ src/models/heco.py
@@ -4,38 +4,40 @@ import torch.nn.functional as F
 
 from .mp_encoder import Mp_encoder
 from .sc_encoder import Sc_encoder
+from copy import deepcopy
+
 
 class HeCo (nn.Module):
-    def __init__ (self, hidden_dim, nodes_type, projection_drop):    
+    def __init__ (self, hidden_dim, nodes_type, projection_drop, attn_drop, P):    
         super(HeCo, self).__init__()
         self.nodes_type = nodes_type
         
         self.node_project = nn.ModuleList([nn.LazyLinear(hidden_dim, bias=True)
-                                         for _ in len(nodes_type)])
+                                         for _ in range(len(nodes_type))])
         
-        for projection in self.node_project:
-            nn.init.xavier_normal_(projection.weight, gain=1.414)
+        #for projection in self.node_project:
+        #    nn.init.xavier_normal_(projection.weight, gain=1.414)
             
         if projection_drop > 0:
             self.feat_drop = nn.Dropout(projection_drop)
         else:
             self.feat_drop = lambda x: x
          
-        self.mp = Mp_encoder()
-        self.sc = Sc_encoder()
+        self.mp = Mp_encoder(P, hidden_dim, attn_drop)
+        self.sc = Sc_encoder(hidden_dim, attn_drop)
         
         self.contrastiveProj = nn.Sequential(
             nn.Linear(hidden_dim, hidden_dim),
             nn.ELU(),
             nn.Linear(hidden_dim, hidden_dim)
         )
-    def forward (self, graph, mode="test"):
+    def forward (self, graph, mps, mode="test"):
         for i, ntype in enumerate(self.nodes_type):
             graph[ntype].x = F.elu(self.feat_drop(
                 self.node_project[i](graph[ntype].x)))
         
-        z_mp = self.mp()
-        z_sc = self.sc()
+        z_mp = self.mp(deepcopy(graph['author'].x), mps)
+        z_sc = self.sc(graph)
         
         if mode == "train":
             z_proj_mp = self.proj(z_mp)
